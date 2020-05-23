10a6b0d019603c6356e22a7392015c3c54c8c4ee
João Vítor Batistella <joaovitor.batistella.ifrs@gmail.com>
2020-05-22 21:07:29 -0300

    iframe created

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -24,62 +24,64 @@
                     <div align="center" class="col-md-12">
                         <h1>Última Notícia</h1>
                     </div>
-
-                        <div class="col-md-8 cfi-first">
-                            <h1>Título da Notícia</h1>
-                            <p style="font-size: 15px">21/05/2020 por João Vítor Batistella</p>
-                            <p>dkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjk</p>
-                            <p style="font-size: 15px"><storng>Fonte:</strong> Rede Crinet</p>
-                        </div>
-                        <div class="col-md-4 cfi-second">
-                            <img src="{{asset('images/teste.jpg')}}" />
-                        </div>
-
+                    <div class="col-md-8 cfi-first">
+                        <h1>Título da Notícia</h1>
+                        <p style="font-size: 15px">21/05/2020 por João Vítor Batistella</p>
+                        <p>dkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjk</p>
+                        <p style="font-size: 15px"><storng>Fonte:</strong> Rede Crinet</p>
+                    </div>
+                    <div class="col-md-4 cfi-second">
+                        <img src="{{asset('images/teste.jpg')}}" />
+                    </div>
                 </div>
             </div>
             <div class="col-md-6 col-two-first-block ">
                 <div class="row">
                     <div class="col-md-12 player-frame">
-                        <img src="{{asset('images/yt_logo.png')}}" />
+                    <iframe width="88%" height="500px" src="https://www.youtube.com/embed/gHhj3JNQKCo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="row content-search-block">
-            <ul class="ul-main">
-                <div class="col-md-4">
-                    <li>
-                        <a href="#">Religião</a>
-                    </li>
-                    <li>
-                        <a href="#">Política</a>
-                    </li>
-                    <li>
-                        <a href="#">Sociedade</a>
-                    </li>
-                </div>
-                <div class="col-md-4">
-                    <li>
-                        <div class="poligonal">
-                            <img src="{{asset('images/logo.png')}}" />
-                        </div>
-                    </li>
-                </div>
-                <div class="col-md-4">
-                    <li>
-                        <a href="#">Saúde</a>
-                    </li>
-                    <li>
-                        <a href="#">Educação</a>
-                    </li>
-                    <li>
-                        <a href="#">Dicas</a>
-                    </li>
-                </div>
-            </ul>
+        <div class="row read-more">
+            <div align="center" class="col-md-12">
+                <h1 style="color: white">Leia mais:</h1>
+            </div>
         </div>
-
+        <div class="row content-search-block">
+                <div class="col-1">
+                    <ul class="ul-left">
+                        <li class="col-li-left">
+                            <a href="#">Religião</a>
+                        </li>
+                        <li class="col-li-left">
+                            <a href="#">Política</a>
+                        </li>
+                        <li class="col-li-left">
+                            <a href="#">Sociedade</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-2">
+                    <div class="poligonal">
+                        <img src="{{asset('images/logo.png')}}" />
+                    </div>
+                </div>
+                <div class="col-3">
+                    <ul class="ul-right">
+                        <li class="col-li-right">
+                            <a href="#">Saúde</a>
+                        </li>
+                        <li class="col-li-right">
+                            <a href="#">Educação</a>
+                        </li>
+                        <li class="col-li-right">
+                            <a href="#">Dicas</a>
+                        </li>
+                    </ul>
+                </div>
+        </div>
+-
         <div class="row content-about-block">
             <div class="col-md-12">
 
