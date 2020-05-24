c0401a39745a4dad7064b6ff0e35c215b69ca65f
João Vítor Batistella <joaovitor.batistella.ifrs@gmail.com>
2020-05-23 21:05:57 -0300

    about finished

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,99 +1,135 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" type="text/css" href="<?php echo asset('css/styled.css')?>">
-    <title>Rede - Crinet</title>
-</head>
-<body>
-    <div class="container-fluid .content-body">
-        <div class="row">
-            <div class="col-md-12 content-logo-bar">
-                <img src="{{asset('images/logo.png')}}" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+        <link rel="stylesheet" type="text/css" href="<?php echo asset('css/styled.css')?>">
+        <title>Rede - Crinet</title>
+    </head>
+    <body>
+        <div class="container-fluid .content-body">
+            <div class="row">
+                <div class="col-md-12 content-logo-bar">
+                    <img id="facebook-main" style="width: 50px" src="{{asset('images/facebook.png')}}" />
+                    <img src="{{asset('images/logo.png')}}" />
+                    <img id="youtube-main" style="width: 70px" src="{{asset('images/yt_logo.png')}}" />
+                </div>
             </div>
-        </div>
 
-        <div class="row content-first-block">
-            <div class="col-md-6 col-one-first-block">
-                <div class="row">
+            <div class="row content-first-block">
+                <div class="col-md-7 col-one-first-block">
                     <div align="center" class="col-md-12">
                         <h1>Última Notícia</h1>
                     </div>
-                    <div class="col-md-8 cfi-first">
-                        <h1>Título da Notícia</h1>
-                        <p style="font-size: 15px">21/05/2020 por João Vítor Batistella</p>
-                        <p>dkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjk</p>
-                        <p style="font-size: 15px"><storng>Fonte:</strong> Rede Crinet</p>
-                    </div>
-                    <div class="col-md-4 cfi-second">
-                        <img src="{{asset('images/teste.jpg')}}" />
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-two-first-block ">
-                <div class="row">
-                    <div class="col-md-12 player-frame">
-                    <iframe width="88%" height="500px" src="https://www.youtube.com/embed/gHhj3JNQKCo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="row">
+                        <div class="col-md-7 cfi-first">
+                            <h1>Título da Notícia</h1>
+                            <p style="font-size: 15px">21/05/2020 por João Vítor Batistella</p>
+                            <p class="retreat">dkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjkdkdlvnsnvsjnjsnjk</p>
+                            <p style="font-size: 15px"><storng>Fonte:</strong> Rede Crinet</p>
+                        </div>
+                        <div class="col-md-5 cfi-second">
+                            <img src="{{asset('images/teste.jpg')}}" />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="row read-more">
-            <div align="center" class="col-md-12">
-                <h1 style="color: white">Leia mais:</h1>
-            </div>
-        </div>
-        <div class="row content-search-block">
-                <div class="col-1">
-                    <ul class="ul-left">
-                        <li class="col-li-left">
-                            <a href="#">Religião</a>
-                        </li>
-                        <li class="col-li-left">
-                            <a href="#">Política</a>
-                        </li>
-                        <li class="col-li-left">
-                            <a href="#">Sociedade</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-2">
-                    <div class="poligonal">
-                        <img src="{{asset('images/logo.png')}}" />
+                <div class="col-md-5 col-two-first-block">
+                    <div class="row">
+                        <div class="col-md-12 player-frame">
+                            <iframe width="100%" height="500px" src="https://www.youtube.com/embed/gHhj3JNQKCo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
                     </div>
                 </div>
-                <div class="col-3">
-                    <ul class="ul-right">
-                        <li class="col-li-right">
-                            <a href="#">Saúde</a>
-                        </li>
-                        <li class="col-li-right">
-                            <a href="#">Educação</a>
-                        </li>
-                        <li class="col-li-right">
-                            <a href="#">Dicas</a>
-                        </li>
-                    </ul>
+            </div>
+            <div class="row read-about">
+                <div align="center" class="col-md-12">
+                    <h1 style="color: white">Leia mais:</h1>
                 </div>
-        </div>
--
-        <div class="row content-about-block">
-            <div class="col-md-12">
-
             </div>
-        </div>
-
-        <div class="row content-supporters-block">
-            <div class="col-md-12">
-
+            <div class="row content-search-block">
+                <div class="col-md-12">
+                    <div class="col-1">
+                        <ul class="ul-left">
+                            <li class="col-li-left">
+                                <a href="#">Religião</a>
+                            </li>
+                            <li class="col-li-left">
+                                <a href="#">Política</a>
+                            </li>
+                            <li class="col-li-left">
+                                <a href="#">Sociedade</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-2">
+                        <div class="poligonal">
+                            <img src="{{asset('images/logo.png')}}" />
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <ul class="ul-right">
+                            <li class="col-li-right">
+                                <a href="#">Saúde</a>
+                            </li>
+                            <li class="col-li-right">
+                                <a href="#">Educação</a>
+                            </li>
+                            <li class="col-li-right">
+                                <a href="#">Dicas</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
+
+            <div class="row content-about-block">
+                <div class="col-md-9 col-about-left">
+                    <h2 class="about-title">A rede Crinet!</h2>
+                    <p class="retreat">
+                    PsadksmdkladklsnfsnsdbfhkbfhksdfjkbsfsdjkbsdfhbsdjkfjsdkfjksdbfksdfjknsdfjknsdjkfhfmsdjnsdfjkbsdfjkbsdfbsdjfjsdfnsjkfnnjsdfnjksdfksdfjksdfjksdfjknsdfjknsdjkfnsdjkfnsjfnjksdfjksdfjksdfsdjkfsdjkfjksdbfjsdkbfjksdbfjksdfjksbfjdksbfjksfjksfjkdsbfkssdfsdnfsdksdfnonsdfonsodfdsdfssfdsfsdfsdfPsadksmdkldsnlsdfjjfnjksdfjksdfjksdfsdjkfsdjkfjksdbfjsdkbfjksdbfjksdfjksbfjdksbfjksfjksfjkdsbfkssdfsdnfsdksdfnonsdfonsodfdsdfssfdsfsdfsdfPsadksmdkldsnlsdfjlfhksfhkfkj
+                    </p>
+                </div>
+                <div class="col-md-3 col-about-right">
+                <figure>
+                    <img src="{{asset('images/retrato.jpg')}}" />
+                    <figcaption>Nome da pessoa</figcaption>
+                </figure>
+                </div>
+            </div>
+
+            <div class="row content-supporters-block">
+                <div class="col-md-9">
+                    <h2 class="about-title">Apoiadores</h2>
+
+                </div>
+                <div class="col-md-3">
+
+                </div>
+            </div>
+
         </div>
 
-    </div>
-</body>
+        <footer>
+            <div class="footer-div">
+                <p><strong>Designed and Built by:</strong> João Vítor Batistella</p>
+                <a target="_blank" href="https://github.com/joaovitorbatistella"><img src="{{asset('images/github_white.png')}}" /><a>
+                <a target="_blank" href="https://linkedin.com/in/joão-vítor-batistella-0aa300175"><img src="{{asset('images/linkedin.png')}}" /><a>
+                <a target="_blank" href="https://facebook.com/joaovitorbatistella.batistellajoviba"><img src="{{asset('images/facebook.png')}}" /><a>
+            </div>
+            <div class="footer-div">
+                <p><strong>Contribution from :</strong> Guilherme Barboza</p>
+                <a class="a-sec" target="_blank" href="https://github.com/"><img src="{{asset('images/github_white.png')}}" /><a>
+                <a class="a-sec" target="_blank" href="https://linkedin.com/"><img src="{{asset('images/linkedin.png')}}" /><a>
+                <a class="a-sec" target="_blank" href="https://facebook.com/"><img src="{{asset('images/facebook.png')}}" /><a>
+            </div>
+        </footer>
+    </body>
 </html>
